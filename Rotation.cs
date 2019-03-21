b824a14edb0c8c446cedb19ce8dd57ee309ecaef
長啓太郎 <48654396+tefutarou@users.noreply.github.com>
2019-03-22 05:25:33 +0900

    Initial commit

---
+++ Rotation.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotation : MonoBehaviour
+{
+    public void Rot()
+    {
+        OnRotate(new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y")));
+    }
+
+    [SerializeField]
+    float RotationSpeed;
+
+    void OnRotate(Vector2 delta)
+    {
+        float deltaAngle = delta.magnitude * RotationSpeed;
+         
+        if (Mathf.Approximately(deltaAngle, 0.0f))
+        {
+            return;
+        }
+
+        Transform cameraTransform = Camera.main.transform;
+        Vector3 deltaWorld = cameraTransform.right * delta.x + cameraTransform.up * delta.y;
+
+       // Vector3 axisWorld = Vector3.Cross(deltaWorld, cameraTransform.forward).normalized;
+        Vector3 axisWorld = transform.up;
+        transform.Rotate(axisWorld, deltaAngle, Space.World);
+    }
+}
+
---
+++ action.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class action : MonoBehaviour
+{
+    GameObject OnMouseObject;
+    GameObject ClickObject;
+    public GameObject parent;
+    public GameObject child;
+    Collider collision;
+    bool a;
+    bool b;
+    bool c;
+    public static bool only;
+
+    Rotation Rotation;
+
+    void Start()
+    {
+        Rotation = parent.GetComponent<Rotation>();
+        parent.transform.DetachChildren();
+        a = false;
+        b = false;
+        c = false;
+        only = false;
+    }
+
+    public void OnMouse()
+    {
+        OnMouseObject = this.gameObject;
+        Debug.Log(OnMouseObject + "を選択しますか？");
+
+        c = true;
+       
+    }
+    public void ExitMouse()
+    {
+        c = false;
+    }
+
+     void Update()
+    {
+        if (only==false&&c==true&&Input.GetMouseButton(0))
+        {
+            Debug.Log(OnMouseObject + "を選択しました。");
+            
+            b = true;
+        }
+        if (Input.GetMouseButton(1))
+        {
+            b = false;
+            a = false;
+            only = false;
+        }
+        if (a == true)
+        {
+            only = true;
+            Rotation.Rot();
+            
+        }
+    }
+
+
+    void OnTriggerStay(Collider collision)
+    {
+        if (b == true)
+        {
+            string layerName = LayerMask.LayerToName(collision.gameObject.layer);
+            if (layerName == "Cube")
+            {
+                child = collision.gameObject;
+                child.transform.parent = parent.transform;
+                a = true;
+                // Debug.Log("接触");
+            }
+        }
+        if (b == false)
+        {
+            parent.transform.DetachChildren();
+           // Debug.Log("解除しました");
+        }
+        
+    }
+
+
+}
